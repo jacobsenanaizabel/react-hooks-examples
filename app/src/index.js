b1f4705be9b8c19ed72d4b4dec1d92eb027ef99b
Ana Izabel <jacobsenanaizabel@gmail.com>
2019-03-11 23:26:48 -0300

    exemmplos de hooks

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { Fragment, Component }  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-import GreetingClass from './App';
+import Section from './helpers/Section';
+import './styles.scss';
+
+import  { Greeting, GreetingClass }  from './hooks/useStateExample';
+import { Title, TitleClass }  from './hooks/useEffectExample';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<GreetingClass />, document.getElementById('root'));
+
+const FEATURE_TOGGLE = {
+    IS_USE_EFFECT_VISIBLE: 'UseEffectExample',
+    IS_USE_STATE_VISIBLE: 'UseStateExample'
+}
+
+class App extends Component {
+    state = {
+        isVisible: [
+            FEATURE_TOGGLE.IS_USE_STATE_VISIBLE,
+            FEATURE_TOGGLE.IS_USE_EFFECT_VISIBLE,
+        ],
+    };
+    
+
+    render() {
+        const { isVisible } = this.state;
+
+        return(
+        <Fragment>
+            <Section
+            isVisible={isVisible.includes(FEATURE_TOGGLE.IS_USE_STATE_VISIBLE)}
+            heading="useState"
+            sectionBg="purple-bg"
+            >
+            <div>
+                <h1 className="heading">new way</h1>
+                <Greeting />
+            </div>
+            <div>
+                <h1 className="heading">old way</h1>
+                <GreetingClass />
+            </div>
+            </Section>
+            <Section
+            isVisible={isVisible.includes(FEATURE_TOGGLE.IS_USE_EFFECT_VISIBLE)}
+            heading="useEffect"
+            sectionBg="gainsboro-bg"
+            >
+            <div>
+                <h1 className="heading">new way</h1>
+                <Title />
+            </div>
+            <div>
+                <h1 className="heading">old way</h1>
+                <TitleClass />
+            </div>
+            </Section>
+        </Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
